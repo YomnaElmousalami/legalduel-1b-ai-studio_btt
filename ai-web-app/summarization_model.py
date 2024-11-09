1de03f7117c26b4b4316aee2234e090e45946020
Yomna Elmousalami <yelmo001@odu.edu>
2024-11-09 01:37:48 -0500

    commented ner code to focus on later

--- ai-web-app/summarization_model.py
+++ ai-web-app/summarization_model.py
@@ -1,6 +1,6 @@
 import spacy
 import os
-from date_spacy import find_dates
+#from date_spacy import find_dates
 from spacy.language import Language
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -11,12 +11,12 @@ client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
 @Language.component("openai_summarizer")
 def summarize_text(doc):
     """Custom component that summarizes the text using OpenAI's GPT model."""
-    dates = []
-    for ent in doc.ents:
-        if ent.label_ == 'DATE':
-            dates = dates.append(ent.text)
+    #dates = []
+    #for ent in doc.ents:
+    #    if ent.label_ == 'DATE':
+    #        dates = dates.append(ent.text)
             
-    date_string = " ".join(dates)
+    #date_string = " ".join(dates)
     prompt = (
         "Please accurately take the following document to create a legal chronology." 
         #"Please also use the dates here in this string: \n{date_string}\n" 
@@ -43,7 +43,7 @@ if not spacy.tokens.Doc.has_extension("summary"):
     spacy.tokens.Doc.set_extension("summary", default=None)
 
 nlp.add_pipe("openai_summarizer", last=True)
-nlp.add_pipe('find_dates')
+#nlp.add_pipe('find_dates')
 
 def summarize(text):
     """Summarizes the input text using the OpenAI GPT model."""
